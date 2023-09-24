e3184e55bbab8dea9b437b5b8117fea2e80ed3f1
afisher000 <43121839+afisher000@users.noreply.github.com>
2023-09-24 12:28:36 -0700

    updates

--- Elevation Map Projects/generate_gcode.py
+++ Elevation Map Projects/generate_gcode.py
@@ -24,7 +24,8 @@ model['T'] = 50                                                                 
 # Define gcode_manager
 gm = gcode_manager(folder, model)
 
-# gm.generate_roughing_gcode('roughing.txt', tool_diameter = 0.25*25.4, max_stepdown=4)
-# gm.generate_smoothing_gcode('smoothing.txt', tool_diameter=4, stepover=1)
-# gm.generate_runs_gcode('runs.txt', zdepth=-1)
+gm.generate_roughing_gcode('roughing.txt', tool_diameter = 0.25*25.4, max_stepdown=4)
+gm.generate_smoothing_gcode('smoothing.txt', tool_diameter=4, stepover=1)
+gm.generate_runs_gcode('runs.txt', zdepth=-1)
 gm.generate_image_gcode('signature')
+gm.generate_image_gcode('logo')
